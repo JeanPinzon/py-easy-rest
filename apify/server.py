b1c14a6368243b6bd1b144f5e00127c578bfd8ad
Jean Pinzon <jean.pinzon@corp.globo.com>
2020-12-11 23:30:33 -0300

    Inversing control of repo passing it from setup to app

--- apify/server.py
+++ apify/server.py
@@ -3,136 +3,152 @@ import yaml
 from sanic import Sanic
 from sanic import response
 
-from apify.repos.mongo import MongoRepo
 from apify.repos import DatabaseError
 
 
-def read_api_params_from_yaml():
-    api = None
-
-    with open(r"./api.yaml") as file:
-        api = yaml.load(file, Loader=yaml.FullLoader)
-
-    return api
-
-
-api = read_api_params_from_yaml()
-app = Sanic(api["slug"])
-repo = MongoRepo()
-
-
-def get_query_string_arg(query_string, arg_name):
-    args = query_string.get("page", [])
+class App():
     
-    if len(args) == 1:
-        return args[0]
+    def __init__(self, repo):
+        self._repo = repo
+        self._api = App._read_api_params_from_yaml()
 
-    if len(args) > 1:
-        return args
-    
-    return None
+        self._handlers_without_id = {
+            "GET": self._get,
+            "POST": self._post,
+        }
+
+        self._handlers_with_id = {
+            "GET": self._list,
+            "POST": self._post,
+            "PATCH": self._patch,
+            "PUT": self._put,
+            "DELETE": self._delete,
+        }
+
+        self.app = Sanic(self._api["slug"])
+
+        self.app.add_route(
+            self.handle_without_id, 
+            f"/{self._api['slug']}", 
+            methods=["GET", "POST"]
+        )
+        self.app.add_route(
+            self.handle_with_id, 
+            f"/{self._api['slug']}/<id>", 
+            methods=["GET", "POST", "PUT", "PATCH", "DELETE"]
+        )
+        self.app.add_route(
+            self.get_schema, 
+            f"/{self._api['slug']}/schema", 
+            methods=["GET"]
+        )
+
+    @staticmethod
+    def _read_api_params_from_yaml():
+        api = None
+
+        with open(r"./api.yaml") as file:
+            api = yaml.load(file, Loader=yaml.FullLoader)
+
+        return api
 
 
-def validate(resource):
-    return None
+    @staticmethod
+    def _get_query_string_arg(query_string, arg_name):
+        args = query_string.get("page", [])
+        
+        if len(args) == 1:
+            return args[0]
+
+        if len(args) > 1:
+            return args
+        
+        return None
 
 
-async def post(request, id=None):      
-    errors = validate(request.json)
-
-    if errors:
-        return response.json({"errors": errors}, status=400)
-
-    resource_id = await repo.create(request.json, id)
-    
-    return response.json({"id": resource_id}, status=201)
+    @staticmethod
+    def _validate(resource):
+        return None
 
 
-async def list(request, id):
-    result = await repo.get(id)
-    
-    if result:
+    async def _post(self, request, id=None):      
+        errors = App._validate(request.json)
+
+        if errors:
+            return response.json({"errors": errors}, status=400)
+
+        resource_id = await self._repo.create(request.json, id)
+
+        return response.json({"id": resource_id}, status=201)
+
+
+    async def _list(self, request, id):
+        result = await self._repo.get(id)
+        
+        if result:
+            return response.json(result)
+
+        return response.json({}, status=404)
+
+
+    async def _get(self, request):
+        page = App._get_query_string_arg(request.args, "page")
+        size = App._get_query_string_arg(request.args, "size")
+
+        result = await self._repo.list(page, size)
+
         return response.json(result)
 
-    return response.json({}, status=404)
+
+    async def _put(self, request, id):
+        errors = App._validate(request.json)
+
+        if errors:
+            return response.json({"errors": errors}, status=400)
+
+        resource_id = await self._repo.replace(id, request.json)
+
+        if resource_id:
+            return response.json({"id": resource_id})
+
+        return response.json({}, status=404)
 
 
-async def get(request):
-    page = get_query_string_arg(request.args, "page")
-    size = get_query_string_arg(request.args, "size")
+    async def _patch(self, request, id):
+        errors = App._validate(request.json)
 
-    result = await repo.list(page, size)
+        if errors:
+            return response.json({"errors": errors}, status=400)
 
-    return response.json(result)
+        resource_id = await self._repo.update(id, request.json)
 
+        if resource_id:
+            return response.json({"id": resource_id})
 
-async def put(request, id):
-    errors = validate(request.json)
-
-    if errors:
-        return response.json({"errors": errors}, status=400)
-
-    resource_id = await repo.replace(id, request.json)
-
-    if resource_id:
-        return response.json({"id": resource_id})
-
-    return response.json({}, status=404)
+        return response.json({}, status=404)
 
 
-async def patch(request, id):
-    errors = validate(request.json)
-
-    if errors:
-        return response.json({"errors": errors}, status=400)
-
-    resource_id = await repo.update(id, request.json)
-
-    if resource_id:
-        return response.json({"id": resource_id})
-
-    return response.json({}, status=404)
+    async def _delete(self, request, id):
+        await self._repo.delete(id)
+        return response.json()
 
 
-async def delete(request, id):
-    await repo.delete(id)
-    return response.json()
+    async def _handle_request(self, handlers, request, *args):
+        try:
+            return await handlers[request.method](request, args)
+        except DatabaseError as db_error:
+            return response.json({"message": db_error.user_message}, status=500)
+        except Exception as error:
+            return response.json({}, status=500)
 
 
-handlers_without_id = {
-    "GET": get,
-    "POST": post,
-}
-
-handlers_with_id = {
-    "GET": list,
-    "POST": post,
-    "PATCH": patch,
-    "PUT": put,
-    "DELETE": delete,
-}
+    async def handle_without_id(self, request):
+        return await self._handle_request(self._handlers_without_id, request)
 
 
-async def handle_request(handlers, request, *args):
-    try:
-        return await handlers[request.method](request, args)
-    except DatabaseError as db_error:
-        return response.json({"message": db_error.user_message}, status=500)
-    except Exception as error:
-        return response.json({}, status=500)
+    async def handle_with_id(self, request, id):
+        return await self._handle_request(self._handlers_with_id, request, id)
 
 
-@app.route(f"/{api['slug']}", methods=["GET", "POST"])
-async def handler_without_id(request):
-    return await handle_request(handlers_without_id, request)
-
-
-@app.route(f"/{api['slug']}/<id>", methods=["GET", "POST", "PUT", "PATCH", "DELETE"])
-async def handler_with_id(request, id):
-    return await handle_request(handlers_with_id, request, id)
-
-
-@app.route(f"/{api['slug']}/schema", methods=["GET"])
-async def get_schema(request):
-    print(request.method)
-    return response.json(api["schema"])
+    async def get_schema(self, request):
+        return response.json(self._api["schema"])
--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from apify.server import app
+from apify.server import App
+from apify.repos.mongo import MongoRepo
 from apify import settings
 
 
 if __name__ == '__main__':
-    app.run(
+    apifyApp = App(MongoRepo())
+    apifyApp.app.run(
         host='0.0.0.0', 
         port=8000, 
         debug=settings.DEBUG, 
